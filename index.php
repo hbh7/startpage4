c42147dd9574651ed8d1e2b181d0e8aed21fcf2c
hbh7 <hbh7@hbh7.com>
2016-11-02 08:56:17 -0400

    Finish!

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<title>Start</title>
+<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+<meta name="theme-color" content="#222">
+<link rel="stylesheet" href="styles.css">
+<script async src="script.js"></script>
+<script src="csi.min.js" type="text/javascript"></script>
+
+<div class="container">
+   <div id="time" type="text">Time</div>
+   <div id="date" type="text"></div>
+
+   <form action="http://www.google.com/search"><input class="textbox" name="q" placeholder="Search" onenter="send()" autocomplete="on" maxlength="60" autofocus></form>
+</div>
+<div class="section">
+   <div class="list red">
+      <h1>Reddit</h1>
+      <ul>
+        <li><a href="https://reddit.com">Frontpage</a></li>
+        <li><a href="https://reddit.com/r/Homelab">/r/Homelab</a></li>
+        <li><a href="https://reddit.com/r/datahoarder">/r/DataHoarder</a></li>
+        <li><a href="https://reddit.com/r/jokes">/r/Jokes</a></li>
+        <li><a href="https://reddit.com/r/AskReddit">/r/AskReddit</a></li>
+        <li><a href="https://reddit.com/r/IDontWorkHereLady">/r/IDontWorkHereLady</a></li>
+        <li><a href=""></a></li>
+      </ul>
+   </div>
+   <div class="list red">
+      <h1>Reddit moderation</h1>
+      <ul>
+        <li><a href="https://reddit.com/r/TalesFromRetail">/r/TalesFromRetail</a></li>
+        <li><a href="https://reddit.com/r/TalesFromTechSupport">/r/TalesFromTechSupport</a></li>
+        <li><a href="https://reddit.com/r/TalesFromThePizzaGuy">/r/TalesFromThePizzaGuy</a></li>
+        <li><a href="https://reddit.com/r/Jailbreak">/r/Jailbreak</a></li>
+        <li><a href="https://reddit.com/r/Startpages">/r/Startpages</a></li>
+        <li><a href="https://reddit.com/r/TIFU">/r/TIFU</a></li>
+        <li><a href=""></a></li>
+      </ul>
+   </div>
+   <div class="list red">
+      <h1>Information</h1>
+      <ul>
+        <li><a href="http://arstechnica.com">Ars Technica</a></li>
+        <li><a href="http://news.ycombinator.com">Hacker News</a></li>
+        <li><a href="http://www.gmail.com">Gmail</a></li>
+        <li><a href="https://www.icloud.com/#reminders">iCloud Reminders</a></li>
+        <li><a href="https://github.com/hbh7?tab=repositories">hbh7's Github Repos</a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+      </ul>
+   </div>
+   <div class="list red">
+      <h1>Useful</h1>
+      <ul>
+        <li><a href="https://www.dominos.com/en/">Domino's Pizza</a></li>
+        <li><a href="https://www.dropbox.com/s/8xbpp2d0b9x8mql/wish%20list.txt">My Wish List</a></li>
+        <li><a href="https://github.com/hbh7/startpage">This Startpage on Github</a></li>
+        <li><a href="?run=true" ;="">Refresh</a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+      </ul>
+   </div>
+   <div class="list red">
+      <h1>Projects</h1>
+      <ul>
+        <li><a class="tab tab4" href="http://school.hbh7.com/westwardexpansion">hbh7 - Westward Expansion</a></li>
+        <li><a class="tab tab4" href="https://hbh7.com/wordpress/">hbh7 - Random Wordpress</a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+      </ul>
+   </div>
+   <div class="list red">
+      <h1>School</h1>
+      <ul>
+        <li><a class="tab tab6" href="http://pathways.finalsite.com/">Pathways</a></li>
+        <li><a class="tab tab6" href="https://powerschool.hartfordschools.org/public/home.html">Powerschool</a></li>
+        <li><a class="tab tab6" href="https://docs.google.com">Google Docs</a>
+        <li><a class="tab tab6" href="https://drive.google.com">Google Drive</a></li>
+        <li><a class="tab tab6" href="https://www.connexus.com/login.aspx">Connexus</a>
+        <li><a class="tab tab6" href="https://translate.google.com/">Google Translate</a></li>
+        <li><a class="tab tab6" href="https://connection.naviance.com/family-connection/auth/login/?hsid=hphspatd">Naviance</a></li>
+      </ul>
+   </div>
+   <div class="list red">
+      <h1>Hobby</h1>
+      <ul>
+        <li><a class="tab tab3" href="http://www.twitch.tv/monstercat">Monstercat Twitch</a></li>
+        <li><a class="tab tab3" href="https://hbh7.com/p/">Glype Proxy</a></li>
+        <li><a class="tab tab3" href="https://github.com/">Github</a></li>
+        <li><a class="tab tab3" href="https://amazon.com/">Amazon</a></li>
+        <li><a class="tab tab3" href="https://ebay.com/">eBay</a></li>
+        <li><a class="tab tab3" href="http://youtube.com">Youtube</a></li>
+        <li><a class="tab tab3" href="http://vessel.com">Vessel</a></li>
+      </ul>
+   </div>
+   <div class="list red">
+      <h1>Good Memes</h1>
+      <ul>
+        <li><a class="tab tab5" href="http://www.food.com/recipe/best-meatballs-203145">The Best Meatballs</a></li>
+        <li><a class="tab tab5" href="http://uniqcode.com/typewriter/">Typewriter Simulator</a>
+        <li><a class="tab tab5" href="http://webwit.nl/input/kbsim/">Model M Simulator</a></li>
+        <li><a class="tab tab5" href="http://addictivearcade.weebly.com/">Addictive Arcade</a></li>
+      </ul>
+   </div>
+   <div class="list red">
+      <h1>Finance</h1>
+      <ul>
+         <li><a href="https://paypal.com">PaypPal</a>
+         <li><a href=""></a></li>
+         <li><a href=""></a></li>
+         <li><a href=""></a></li>
+      </ul>
+   </div>
+   <div class="list red">
+      <h1>Scratchpad</h1>
+      <div class="scratchpadOne">
+         <div class="scratchpadTwo" id="scratchpad" contenteditable=true style="background-color: #444; outline-color: rgba(0,0,0,0);">
+             Write some notes...
+         </div>
+       </div>
+   </div>
+</div>
+<div id="footer" style="text-align: center; width: 100%; margin-bottom: 2%;">
+  <div style="background-color: #333; width: 200px; margin: 0 auto; border-radius: 25px; height: 67px;">
+    <div style="display:inline" id="temperature" style="text"></div><div style="display:inline"><img id="conditions" src=""></div>
+  </div>
+</div>
+
+<script type="text/javascript">
+
+      var scratch = document.getElementById('scratchpad');
+      scratch.addEventListener('keyup', function() {
+          var content = scratch.innerHTML.trim();
+          localStorage.setItem('scratchpad', content);
+      });
+
+      var content = localStorage.getItem('scratchpad');
+      if(content == '' || content == null) {
+          content = 'Write some notes here...';
+      }
+      scratch.innerHTML = content;
+
+</script>
+
+<?php
+if ($_GET['run']) {
+  # This code will run if ?run=true is set.
+  shell_exec("/var/www/updatestartpage");
+}
+?>
